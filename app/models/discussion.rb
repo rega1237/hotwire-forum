8c7a2f103ff20e8c2538c172a7177bc2b52b2279
rega1237 <rega1237@gmail.com>
2023-01-04 17:12:18 -0400

    to param id and turbo stream broadcast discussion model

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,5 +1,13 @@
 class Discussion < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true
+  validates :title, presence: true
+
+  after_create_commit -> { broadcast_append_to "discussions" }
+  after_update_commit -> { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
+
+  def to_param
+    "#{id}-#{title.downcase.to_s[0..100]}".parameterize
+  end
 end
