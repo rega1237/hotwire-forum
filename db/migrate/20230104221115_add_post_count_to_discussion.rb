c0b92d7f9dd705267789fb571cfbe45d5358cadc
rega1237 <rega1237@gmail.com>
2023-01-06 12:56:12 -0400

    add post count to discussion

--- db/migrate/20230104221115_add_post_count_to_discussion.rb
+++ db/migrate/20230104221115_add_post_count_to_discussion.rb
@@ -1,5 +1,5 @@
 class AddPostCountToDiscussion < ActiveRecord::Migration[7.0]
   def change
-    add_column :discussions, :post_count, :integer, default: 0
+    add_column :discussions, :posts_count, :integer, default: 0
   end
 end
