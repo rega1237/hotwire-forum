f5ebb721022e5ab91e72d2b90e6a4e840b1650c5
rega1237 <rega1237@gmail.com>
2023-01-06 12:55:29 -0400

    add current atribute with active support for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user, if: :user_signed_in?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name last_name username])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name last_name username])
+  end
+
+  private
+
+  def set_current_user
+    Current.user = current_user
   end
 end
