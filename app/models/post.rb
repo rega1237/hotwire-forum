045f617dc9c4b0ec2412062a594e71b12930fcb7
rega1237 <rega1237@gmail.com>
2023-02-15 23:21:32 -0400

    multiline lambda

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,12 @@ class Post < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   has_rich_text :body
 
-  after_create_commit -> { broadcast_append_later_to discussion, partial: 'discussions/posts/post', locals: { post: self } }
-  after_update_commit -> { broadcast_replace_later_to discussion, partial: 'discussions/posts/post', locals: { post: self } }
+  after_create_commit lambda {
+                        broadcast_append_later_to discussion, partial: 'discussions/posts/post', locals: { post: self }
+                      }
+  after_update_commit lambda {
+                        broadcast_replace_later_to discussion, partial: 'discussions/posts/post', locals: { post: self }
+                      }
   after_destroy_commit -> { broadcast_remove_later_to discussion }
 
   validates :body, presence: true
