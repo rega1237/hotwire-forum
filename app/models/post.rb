e2f4fee908b36d7c002e3caa009cd63937b7e558
rega1237 <rega1237@gmail.com>
2023-02-18 20:27:07 -0400

    install noticed gem and add new post notification

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :discussion, counter_cache: true, touch: true
   belongs_to :user, default: -> { Current.user }
   has_rich_text :body
+  has_noticed_notifications
 
   after_create_commit lambda {
                         broadcast_append_later_to discussion, partial: 'discussions/posts/post', locals: { post: self }
