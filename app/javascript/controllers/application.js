a8edf049e77f6387576244be3ee35bb7460b2e61
rega1237 <rega1237@gmail.com>
2023-02-23 19:50:13 -0400

    mark as read method in the controller and fetch the route to run the method on click

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,7 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+import NotificationController from "./notifications_controller"
+application.register("notifications", NotificationController)
+
 export { application }
